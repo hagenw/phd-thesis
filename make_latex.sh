4abf32105010fbf8a0e5700df073a89bf81def05
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2016-02-29 15:35:21 +0100

    Add files for compilation

---
+++ make_latex.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# --- C O N F I G U R A T I O N ---------
+FILE=wierstorf2014-perceptual_assessment_of_sound_field_synthesis
+FOLDERS="01_introduction \
+02_sound_field_synthesis \
+03_binaural_synthesis \
+04_psychoacoustics \
+05_modelling \
+06_quality"
+# --- M A I N ---------------------------
+# check if we have to add an extra latex run
+if ! [[ -f $FILE.aux ]]
+then
+    latex $FILE
+fi
+if ! [[ -f $FILE.bbl ]]
+then
+    biber $FILE
+fi
+# run latex
+latex $FILE
+dvipdf $FILE
+# apply metadata (this has to be done after the compilation of the PDF, because
+# the gnuplot figures overwrites the hyperref settings)
+mv $FILE.pdf ${FILE}_tmp.pdf
+pdftk ${FILE}_tmp.pdf \
+    update_info metadata.txt \
+    output $FILE.pdf
+rm ${FILE}_tmp.pdf
