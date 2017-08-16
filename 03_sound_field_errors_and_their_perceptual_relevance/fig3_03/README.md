a3f39c40e6da8d9b48ed300d1cd9e2b8c20a2086
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2017-08-16 12:46:28 +0100

    Add pdf version of Fig. 3.3

--- 03_sound_field_errors_and_their_perceptual_relevance/fig3_03/README.md
+++ 03_sound_field_errors_and_their_perceptual_relevance/fig3_03/README.md
@@ -1,19 +1,18 @@
 ![Fig 3.3](fig3_03.png)
 
-**Figure 3.3**: Sound pressure in decibel of a plane wave synthesized with
-2.5D WFS (2.55). The result of an
-infinite linear secondary source distribution is compared with two truncated
-ones. Parameters: n_k = (0,−1,0),
-x_ref = (0,−2,0) m f = 3 kHz.
+**Figure 3.3**: Sound pressure in decibel of a plane wave synthesized with 2.5D
+WFS (2.55). The result of an infinite linear secondary source distribution is
+compared with two truncated ones. Parameters: n_k = (0,−1,0), x_ref = (0,−2,0) m
+f = 3 kHz.
 
 ## Steps for reproduction
 
 Matlab/Octave:
 ```Matlab
->> sound_field_truncation
+>> fig3_03
 ```
 
 Bash:
 ```Bash
-$ gnuplot sound_field_truncation.gnu
+$ gnuplot fig3_03.plt
 ```
--- 03_sound_field_errors_and_their_perceptual_relevance/fig3_03/fig3_03.m
+++ 03_sound_field_errors_and_their_perceptual_relevance/fig3_03/fig3_03.m
@@ -1,7 +1,8 @@
 % create sound field simulations for a linear array and WFS with and without
 % tapering window to show the edge waves
 clear all;
-
+addpath('../../matlab');
+create_dir('data');
 
 %% ===== Configuration ===================================================
 X = [-2.1 2.1]; % / m
@@ -10,7 +11,6 @@ Z = 0; % / m
 f = 3000; % / Hz
 xs = [0 -1 0];
 src = 'pw';
-
 
 %% ===== Toolbox settings ================================================
 conf.resolution = 1000; % / samples
@@ -25,23 +25,20 @@ conf.usetapwin = false;
 conf.tapwinlen = 0.3;
 conf.showprogress = true;
 
-
 %% ===== Secondary Sources ===============================================
 conf.secondary_sources.center = [0 0 0]; % / m
 conf.secondary_sources.geometry = 'line';
 conf.secondary_sources.number = 100;
 conf.secondary_sources.x0 = [];
 
-
 %% ===== Without tapering ================================================
 conf.secondary_sources.size = 1.8; % / m
 conf.secondary_sources.number = 900;
 x0 = secondary_source_positions(conf);
 conf.secondary_sources.x0 = x0;
-gp_save_loudspeakers('array_truncated.txt',x0);
+gp_save_loudspeakers('data/array_truncated.txt',x0);
 [P,x,y] = sound_field_mono_wfs(X,Y,Z,xs,src,f,conf);
-gp_save_matrix('sound_field_truncated.dat',x,y,db(abs(P)));
-
+gp_save_matrix('data/sound_field_truncated.dat',x,y,db(abs(P)));
 
 %% ===== Truncated with round edge =======================================
 % create a circle
@@ -62,10 +59,9 @@ x0 = [x0_circ(472:-1:236,:); ...
     x0_line; ...
     bsxfun(@plus,x0_circ(236:-1:1,:),[1.2 0 0 0 0 0 0])];
 conf.secondary_sources.x0 = x0;
-gp_save_loudspeakers('array_truncated_round.txt',x0);
+gp_save_loudspeakers('data/array_truncated_round.txt',x0);
 [P,x,y] = sound_field_mono_wfs(X,Y,Z,xs,src,f,conf);
-gp_save_matrix('sound_field_truncated_round.dat',x,y,db(abs(P)));
-
+gp_save_matrix('data/sound_field_truncated_round.dat',x,y,db(abs(P)));
 
 %% ===== With tapering ===================================================
 conf.secondary_sources.x0 = [];
@@ -73,6 +69,6 @@ conf.secondary_sources.size = 1000; % / m
 conf.secondary_sources.number = 100000;
 x0 = secondary_source_positions(conf);
 conf.secondary_sources.x0 = x0;
-gp_save_loudspeakers('array_inf.txt',x0);
+gp_save_loudspeakers('data/array_inf.txt',x0);
 [P,x,y] = sound_field_mono_wfs(X,Y,Z,xs,src,f,conf);
-gp_save_matrix('sound_field_inf.dat',x,y,db(abs(P)));
+gp_save_matrix('data/sound_field_inf.dat',x,y,db(abs(P)));
