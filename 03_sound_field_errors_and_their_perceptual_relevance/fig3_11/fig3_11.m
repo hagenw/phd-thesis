bc01b0a5ee38adcb4a4c9aa97763e2f9d35e9afc
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2017-08-16 14:26:42 +0100

    Add pdf version of Fig. 3.11

--- 03_sound_field_errors_and_their_perceptual_relevance/fig3_11/fig3_11.m
+++ 03_sound_field_errors_and_their_perceptual_relevance/fig3_11/fig3_11.m
@@ -1,6 +1,7 @@
 % create the data for the monochromatic sound field plots
 clear all
-
+addpath('../../matlab');
+create_dir('data');
 
 %% ===== Configuration ===================================================
 X = [-1.75 1.75]; % / m
@@ -8,7 +9,6 @@ Y = [-1.75 1.75]; % / m
 Z = 0; % / m
 xs = [0 -1 0];
 src = 'pw';
-
 
 %% ===== Toolbox settings ================================================
 conf.resolution = 1000; % / samples
@@ -24,7 +24,6 @@ conf.tapwinlen = 0.3;
 conf.showprogress = true;
 conf.nfchoa.order = [];
 
-
 %% ===== Secondary Sources ===============================================
 conf.secondary_sources.size = 3; % / m
 conf.secondary_sources.center = [0 0 0]; % / m
@@ -32,18 +31,16 @@ conf.secondary_sources.geometry = 'circle';
 conf.secondary_sources.number = 64;
 conf.secondary_sources.x0 = [];
 
-
 %% ===== without aliasing ================================================
 f = 1000; % / Hz
 [P,x,y,~,x0] = sound_field_mono_nfchoa(X,Y,Z,xs,src,f,conf);
-gp_save_matrix('sound_field_nfchoa_1000Hz.dat',x,y,real(P));
-gp_save_loudspeakers('array.txt',x0);
-
+gp_save_matrix('data/sound_field_nfchoa_1000Hz.dat',x,y,real(P));
+gp_save_loudspeakers('data/array.txt',x0);
 
 %% ===== with aliasing ===================================================
 f = 2000; % / Hz
 [P,x,y] = sound_field_mono_nfchoa(X,Y,Z,xs,src,f,conf);
-gp_save_matrix('sound_field_nfchoa_2000Hz.dat',x,y,real(P));
+gp_save_matrix('data/sound_field_nfchoa_2000Hz.dat',x,y,real(P));
 f = 5000; % / Hz
 [P,x,y] = sound_field_mono_nfchoa(X,Y,Z,xs,src,f,conf);
-gp_save_matrix('sound_field_nfchoa_5000Hz.dat',x,y,real(P));
+gp_save_matrix('data/sound_field_nfchoa_5000Hz.dat',x,y,real(P));
