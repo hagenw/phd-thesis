0f38cebc4da1eef05f4e246fd3c2b5cfac20ea2a
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2017-08-16 14:15:48 +0100

    Add pdf version of Fig. 3.9

--- 03_sound_field_errors_and_their_perceptual_relevance/fig3_09/fig3_09.m
+++ 03_sound_field_errors_and_their_perceptual_relevance/fig3_09/fig3_09.m
@@ -1,5 +1,7 @@
 % create the data for the monochromatic sound field plots
 clear all
+addpath('../../matlab');
+create_dir('data');
 
 %% ===== Configuration ===================================================
 X = [-1.75 1.75]; % / m
@@ -7,7 +9,6 @@ Y = [-1.75 1.75]; % / m
 Z = 0; % / m
 xs = [0 0.5 0 0 -1 0];
 src = 'fs';
-
 
 %% ===== Toolbox settings ================================================
 conf.resolution = 1000; % / samples
@@ -22,7 +23,6 @@ conf.usetapwin = false;
 conf.tapwinlen = 0.3;
 conf.showprogress = true;
 
-
 %% ===== Secondary Sources ===============================================
 conf.secondary_sources.size = 3; % / m
 conf.secondary_sources.center = [0 0 0]; % / m
@@ -30,20 +30,18 @@ conf.secondary_sources.geometry = 'circle';
 conf.secondary_sources.number = 64;
 conf.secondary_sources.x0 = [];
 x0 = secondary_source_positions(conf);
-gp_save_loudspeakers('array.txt',x0);
-
+gp_save_loudspeakers('data/array.txt',x0);
 
 %% ===== without aliasing ================================================
 f = 1000; % / Hz
 [P,x,y,~,x0] = sound_field_mono_wfs(X,Y,Z,xs,src,f,conf);
-gp_save_matrix('sound_field_fs_1000Hz.dat',x,y,real(P));
-gp_save_loudspeakers('array_wfs.txt',x0);
-
+gp_save_matrix('data/sound_field_fs_1000Hz.dat',x,y,real(P));
+gp_save_loudspeakers('data/array_wfs.txt',x0);
 
 %% ===== with aliasing ===================================================
 f = 2000; % / Hz
 [P,x,y] = sound_field_mono_wfs(X,Y,Z,xs,src,f,conf);
-gp_save_matrix('sound_field_fs_2000Hz.dat',x,y,real(P));
+gp_save_matrix('data/sound_field_fs_2000Hz.dat',x,y,real(P));
 f = 5000; % / Hz
 [P,x,y] = sound_field_mono_wfs(X,Y,Z,xs,src,f,conf);
-gp_save_matrix('sound_field_fs_5000Hz.dat',x,y,real(P));
+gp_save_matrix('data/sound_field_fs_5000Hz.dat',x,y,real(P));
