e2e2c70d68e3b59afaa1ccb53df8b1d8187b4051
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2017-08-16 14:11:52 +0100

    Add pdf version of Fig. 3.8

--- 03_sound_field_errors_and_their_perceptual_relevance/fig3_08/README.md
+++ 03_sound_field_errors_and_their_perceptual_relevance/fig3_08/README.md
@@ -1,19 +1,18 @@
 ![Fig 3.8](fig3_08.png)
 
-**Figure 3.8**: Sound pressure of a plane
-wave synthesized by NFC-HOA (2.45)
-and WFS (2.55) for different frequencies.
-For WFS the open circles indicate inactive secondary sources. Parameters:
-x_s = (0,−1,0), x_ref = (0,0,0) m, 64 secondary sources
+**Figure 3.8**: Sound pressure of a plane wave synthesized by NFC-HOA (2.45) and
+WFS (2.55) for different frequencies.  For WFS the open circles indicate
+inactive secondary sources. Parameters: x_s = (0,−1,0), x_ref = (0,0,0) m, 64
+secondary sources
 
 ## Steps for reproduction
 
 Matlab/Octave:
 ```Matlab
->> sound_field_aliasing
+>> fig3_08
 ```
 
 Bash:
 ```Bash
-$ gnuplot sound_field_aliasing.gnu
+$ gnuplot fig3_08.plt
 ```
--- 03_sound_field_errors_and_their_perceptual_relevance/fig3_08/fig3_08.m
+++ 03_sound_field_errors_and_their_perceptual_relevance/fig3_08/fig3_08.m
@@ -1,5 +1,7 @@
 % create the data for the monochromatic sound field plots
 clear all
+addpath('../../matlab');
+create_dir('data');
 
 %% ===== Configuration ===================================================
 X = [-1.75 1.75]; % / m
@@ -7,7 +9,6 @@ Y = [-1.75 1.75]; % / m
 Z = 0; % / m
 xs = [0 -1 0];
 src = 'pw';
-
 
 %% ===== Toolbox settings ================================================
 conf.resolution = 1000; % / samples
@@ -23,7 +24,6 @@ conf.tapwinlen = 0.3;
 conf.showprogress = true;
 conf.nfchoa.order = 100;
 
-
 %% ===== Secondary Sources ===============================================
 conf.secondary_sources.size = 3; % / m
 conf.secondary_sources.center = [0 0 0]; % / m
@@ -31,20 +31,19 @@ conf.secondary_sources.geometry = 'circle';
 conf.secondary_sources.number = 64;
 conf.secondary_sources.x0 = [];
 
-
 %% ===== without aliasing ================================================
 f = 1000; % / Hz
 [P,x,y,~,x0] = sound_field_mono_nfchoa(X,Y,Z,xs,src,f,conf);
-gp_save_matrix('sound_field_nfchoa_1000Hz.dat',x,y,real(P));
-gp_save_loudspeakers('array.txt',x0);
+gp_save_matrix('data/sound_field_nfchoa_1000Hz.dat',x,y,real(P));
+gp_save_loudspeakers('data/array.txt',x0);
 
 
 %% ===== with aliasing ===================================================
 f = 2000; % / Hz
 [P,x,y] = sound_field_mono_nfchoa(X,Y,Z,xs,src,f,conf);
-gp_save_matrix('sound_field_nfchoa_2000Hz.dat',x,y,real(P));
+gp_save_matrix('data/sound_field_nfchoa_2000Hz.dat',x,y,real(P));
 f = 2000; % / Hz
 conf.phase = 0.8*pi/2;
 [P,x,y,~,x0] = sound_field_mono_wfs(X,Y,Z,xs,src,f,conf);
-gp_save_matrix('sound_field_wfs_2000Hz.dat',x,y,real(P));
-gp_save_loudspeakers('array_wfs.txt',x0);
+gp_save_matrix('data/sound_field_wfs_2000Hz.dat',x,y,real(P));
+gp_save_loudspeakers('data/array_wfs.txt',x0);
